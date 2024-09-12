70676dfd7e6d3d493b995af3a47f4053b2b1a6cb
TamaraTegeltija <128434508+TamaraTegeltija@users.noreply.github.com>
2024-09-12 23:34:19 +0200

    Update MH20__2019.py
    Ispravljeno slovo

--- MH20__2019.py
+++ MH20__2019.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Press Double Shif to search everywhere for classes, files, tool windows, actions, and settings.
 import math
 '''
 mat1 = [1, 0, 0, 0], [0, -0.707, 0.707, 0.707], [0, -0.707, -0.707, 1], [0, 0, 0, 1]
